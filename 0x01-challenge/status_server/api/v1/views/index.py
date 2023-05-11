319574f5928d5f4a8ac30e98cd21542b5b4887a4
nerbrain <malikmox@gmail.com>
2023-05-11 10:05:41 +0000

    fixed task 1

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Index view
 """
-from flask import jsonify
+from flask import jsonify, Flask, Blueprint
 
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app_views = Blueprint('app_views', __name__)
 
 @app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
